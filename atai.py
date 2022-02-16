4a4a27b8c610e0ad766ce15d87cf490d2051d2fb
argennurlanuulu <argennurlanov98@gmail.com>
2022-02-16 13:44:33 +0600

    второой комиt

---
+++ atai.py
@@ -0,0 +1,38 @@
+# 1
+
+# num1=int(input("введите число?"))
+# for i in range(1,11):   
+#     print(num1*i)
+
+
+# 2
+
+# num2=int(input('number?'))
+# total=0
+# for i in range(1,num2+1):
+#     total+=i
+# print(total)
+
+#3
+
+# num3 = int (input('numer?'))
+# for i in range(num3,-1):
+#     print(i)
+
+
+#4
+
+# num=-10
+# while num<=-1:
+#     print(num)
+#     num+=1
+
+#5
+
+# num5 = int (input ( ' number ? ' ) )
+# for i in range( 1 , num5+1 ):
+#     print (f'Текущая число{i},куб числа равняется{i**3}')
+
+
+
+
